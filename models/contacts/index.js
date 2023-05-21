a33cf36ba3fb8ec6026163790e93b7eae244a1cb
tooreagen <tooreagen@gmail.com>
2023-05-21 12:36:27 +0300

    refactoring listContacts

--- models/contacts/index.js
+++ models/contacts/index.js
@@ -1,15 +1,12 @@
-const fs = require("fs").promises;
 const { nanoid } = require("nanoid");
-const path = require("path");
-
-const contactsPath = path.join(__dirname, "contacts.json");
+const contactsModel = require("../../server");
 
 const writeContacts = async (contacts) => {
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 };
 
 const listContacts = async () => {
-  const contacts = await fs.readFile(contactsPath);
+  const contacts = await contactsModel.find();
   return JSON.parse(contacts);
 };
 
@@ -19,14 +16,14 @@ const getContactById = async (contactId) => {
 };
 
 const removeContact = async (contactId) => {
-    const contacts = await listContacts();
-    const index = contacts.findIndex((item) => item.id === contactId);
-    if (index === -1) {
-      return null;
-    }
-    const [result] = contacts.splice(index, 1);
-    await writeContacts(contacts);
-    return result;
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await writeContacts(contacts);
+  return result;
 };
 
 const addContact = async (body) => {
@@ -48,7 +45,7 @@ const updateContact = async (id, body) => {
   }
   contacts[index] = { id, ...body };
   await writeContacts(contacts);
-  return contacts[index];;
+  return contacts[index];
 };
 
 module.exports = {
